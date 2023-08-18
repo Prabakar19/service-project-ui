def9df1bedfed500a146d7f7c7fb585a4ee0a40b
Prabakar19 <praphakar119@gmail.com>
2023-08-18 21:29:09 +0530

    modify customer dashboard

--- src/app/services/service-provider-service/service-provider.service.ts
+++ src/app/services/service-provider-service/service-provider.service.ts
@@ -9,6 +9,7 @@ export class ServiceProviderService {
   root = 'api/serviceprovider';
   address = 'api/address/serviceprovider/';
   transaction = 'api/transaction/';
+  meta_api = 'api/meta_api';
 
   constructor(private http: HttpClient) {}
 
@@ -31,7 +32,7 @@ export class ServiceProviderService {
     return this.http.get<any>(this.root + '/servicecount/' + id);
   }
   getSPCities(): Observable<any> {
-    return this.http.get<any>(this.root + '/cities');
+    return this.http.get<any>(this.meta_api + '/cities');
   }
   serviceProviderLoginRequest(data: any): Observable<any> {
     return this.http.post<any>(this.root + '/login', data);
