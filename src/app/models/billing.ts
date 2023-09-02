a8fdb43571e9d9c258f4cc9f12676ea543e6a754
Prabakar19 <praphakar119@gmail.com>
2023-09-02 17:46:19 +0530

    change variable types of billing

--- src/app/models/billing.ts
+++ src/app/models/billing.ts
@@ -1,12 +1,12 @@
 import { Transaction } from './transaction';
 
 export interface Billing {
-  billingId: number;
+  billingId: string;
   cost: number;
   gst: number;
   totalCost: number;
   originalCost: number;
-  customerId: number;
-  serviceProviderId: number;
+  customerId: string;
+  serviceProviderId: string;
   transactions: Transaction[];
 }
